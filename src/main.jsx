ed7e89e42bebf663a72fe6cc23f0455f2219cdad
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-09-23 21:42:49 +0700

    Merge Branch Dante to Main

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import "./index.css";
-import CustomerManagement from "./pages/admin/staff/CustomerManagement";
-import Dashboard from "./pages/admin/dashboard/DashBoard";
 import AdminLayout from "./components/adminComponents/AdminLayout";
+import "./index.css";
+import Dashboard from "./pages/admin/dashboard/DashBoard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Thêm Route Trang vào */}
-        <Route path="/" element={<CustomerManagement />}></Route>
         <Route path="/" element={<AdminLayout />}>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
         </Route>
       </Routes>
