fc57b4ab982854e96f2d5d4975fa5b5beb4ef646
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-09-28 16:47:12 +0700

    Merge code JavisNgo

--- src/pages/staff/table-management/TableManagement.jsx
+++ src/pages/staff/table-management/TableManagement.jsx
@@ -1,16 +1,80 @@
-import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const initialTableData = [
-  { id: 1, name: 'TCA01', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Còn trống' },
-  { id: 2, name: 'TCA02', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Đang phục vụ' },
-  { id: 3, name: 'TCA03', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Còn trống' },
-  { id: 4, name: 'TCA04', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Còn trống' },
-  { id: 5, name: 'TCA05', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Đang phục vụ' },
-  { id: 6, name: 'TCA06', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Còn trống' },
-  { id: 7, name: 'TCA07', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Còn trống' },
-  { id: 8, name: 'TCA08', type: 'Bàn Tiêu chuẩn 1', price: '600.000 VND', minGuests: 2, maxGuests: 4, status: 'Còn trống' },
+  {
+    id: 1,
+    name: "TCA01",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Còn trống",
+  },
+  {
+    id: 2,
+    name: "TCA02",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Đang phục vụ",
+  },
+  {
+    id: 3,
+    name: "TCA03",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Còn trống",
+  },
+  {
+    id: 4,
+    name: "TCA04",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Còn trống",
+  },
+  {
+    id: 5,
+    name: "TCA05",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Đang phục vụ",
+  },
+  {
+    id: 6,
+    name: "TCA06",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Còn trống",
+  },
+  {
+    id: 7,
+    name: "TCA07",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Còn trống",
+  },
+  {
+    id: 8,
+    name: "TCA08",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "600.000 VND",
+    minGuests: 2,
+    maxGuests: 4,
+    status: "Còn trống",
+  },
 ];
 
 function TableManagement() {
@@ -18,15 +82,15 @@ function TableManagement() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [searchTerm, setSearchTerm] = useState(''); // New state for search term
+  const [searchTerm, setSearchTerm] = useState(""); // New state for search term
   const [currentTable, setCurrentTable] = useState({
     id: null,
-    name: '',
-    type: 'Bàn Tiêu chuẩn 1',
-    price: '',
+    name: "",
+    type: "Bàn Tiêu chuẩn 1",
+    price: "",
     minGuests: 1,
     maxGuests: 1,
-    status: 'Còn trống',
+    status: "Còn trống",
   });
   const [tableToDelete, setTableToDelete] = useState(null);
 
@@ -42,12 +106,12 @@ function TableManagement() {
   const openModal = () => {
     setCurrentTable({
       id: null,
-      name: '',
-      type: 'Bàn Tiêu chuẩn 1',
-      price: '',
+      name: "",
+      type: "Bàn Tiêu chuẩn 1",
+      price: "",
       minGuests: 1,
       maxGuests: 1,
-      status: 'Còn trống',
+      status: "Còn trống",
     });
     setIsModalOpen(true);
     setIsEditing(false);
@@ -75,7 +139,9 @@ function TableManagement() {
   const handleSaveTable = () => {
     if (isEditing) {
       setTableData((prevData) =>
-        prevData.map((table) => (table.id === currentTable.id ? { ...currentTable } : table))
+        prevData.map((table) =>
+          table.id === currentTable.id ? { ...currentTable } : table
+        )
       );
     } else {
       setTableData((prevData) => [
@@ -87,7 +153,9 @@ function TableManagement() {
   };
 
   const handleConfirmDelete = () => {
-    setTableData((prevData) => prevData.filter((table) => table.id !== tableToDelete.id));
+    setTableData((prevData) =>
+      prevData.filter((table) => table.id !== tableToDelete.id)
+    );
     setTableToDelete(null);
     closeDeletePopup();
   };
@@ -97,15 +165,23 @@ function TableManagement() {
       <div className="flex gap-5 max-md:flex-col">
         <main className="flex flex-col w-full max-md:ml-0 max-md:w-full">
           <div className="flex overflow-hidden px-8 flex-col pb-72 w-full bg-white max-md:px-5 max-md:pb-24">
-            <TableHeader openModal={openModal} searchTerm={searchTerm} onSearchChange={handleSearchChange} />
+            <TableHeader
+              openModal={openModal}
+              searchTerm={searchTerm}
+              onSearchChange={handleSearchChange}
+            />
             <section className="w-full mt-10">
               <div className="grid grid-cols-8 gap-4 px-5 w-full font-semibold bg-white text-sm min-h-[40px] text-neutral-900 max-md:text-xs">
                 <div className="text-center break-words">ID Bàn</div>
                 <div className="text-center break-words">Tên Bàn</div>
                 <div className="text-center break-words">Loại bàn</div>
                 <div className="text-center break-words">Mức giá tối thiểu</div>
-                <div className="text-center break-words">Lượng khách tối thiểu</div>
-                <div className="text-center break-words">Lượng khách tối đa</div>
+                <div className="text-center break-words">
+                  Lượng khách tối thiểu
+                </div>
+                <div className="text-center break-words">
+                  Lượng khách tối đa
+                </div>
                 <div className="text-center break-words">Trạng thái</div>
                 <div className="text-center break-words">Hành động</div>
               </div>
@@ -125,7 +201,7 @@ function TableManagement() {
               <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
                 <div className="bg-white rounded-lg p-8 w-[500px]">
                   <h2 className="text-2xl font-semibold mb-6">
-                    {isEditing ? 'Chỉnh sửa bàn' : 'Thêm bàn mới'}
+                    {isEditing ? "Chỉnh sửa bàn" : "Thêm bàn mới"}
                   </h2>
 
                   <div className="mb-4">
@@ -161,7 +237,7 @@ function TableManagement() {
                       className="bg-blue-600 text-white py-3 w-48 rounded-full"
                       onClick={handleSaveTable}
                     >
-                      {isEditing ? 'Lưu' : 'Thêm'}
+                      {isEditing ? "Lưu" : "Thêm"}
                     </button>
                   </div>
                 </div>
@@ -173,7 +249,10 @@ function TableManagement() {
               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
                 <div className="bg-white p-6 rounded-lg shadow-lg w-[480px]">
                   <h2 className="text-2xl font-bold mb-4">Xác nhận xóa</h2>
-                  <p>Bạn có chắc chắn muốn xóa bàn: <strong>{tableToDelete?.name}</strong>?</p>
+                  <p>
+                    Bạn có chắc chắn muốn xóa bàn:{" "}
+                    <strong>{tableToDelete?.name}</strong>?
+                  </p>
                   <div className="flex justify-between mt-6">
                     <button
                       className="bg-gray-400 text-white py-3 w-48 rounded-full"
@@ -210,7 +289,7 @@ function TableHeader({ openModal, searchTerm, onSearchChange }) {
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/8193c2c7a19b0a3b80ee04ee6c2fd4a3239559cb76a7c142500d11b564d9c3ba?placeholderIfAbsent=true&apiKey=51ebf0c031414fe7a365d6657293527e"
           className="object-contain cursor-pointer shrink-0 w-8 aspect-square"
           alt="icon"
-          onClick={() => navigate('/staff/table-management')}
+          onClick={() => navigate("/staff/table-management")}
         />
         <h3 className="basis-auto">Bàn Tiêu chuẩn 1</h3>
       </div>
@@ -249,12 +328,24 @@ TableHeader.propTypes = {
   onSearchChange: PropTypes.func.isRequired,
 };
 
-function TableRow({ id, name, type, price, minGuests, maxGuests, status, isEven, openEditModal, openDeletePopup }) {
+function TableRow({
+  id,
+  name,
+  type,
+  price,
+  minGuests,
+  maxGuests,
+  status,
+  isEven,
+  openEditModal,
+  openDeletePopup,
+}) {
   const rowClass = isEven
     ? "grid grid-cols-8 gap-4 px-5 py-4 w-full bg-orange-50 border-b border-gray-200 text-sm min-h-[60px] break-words"
     : "grid grid-cols-8 gap-4 px-5 py-4 w-full bg-white border-b border-orange-200 text-sm min-h-[60px] break-words";
 
-  const statusClass = status === 'Còn trống' ? 'text-green-500' : 'text-yellow-500';
+  // Set status class with background color based on the status
+  const statusClass = status === "Còn trống" ? "bg-green-500" : "bg-yellow-500";
 
   return (
     <div className={rowClass}>
@@ -264,13 +355,29 @@ function TableRow({ id, name, type, price, minGuests, maxGuests, status, isEven,
       <div className="text-center font-normal">{price}</div>
       <div className="text-center font-normal">{minGuests}</div>
       <div className="text-center font-normal">{maxGuests}</div>
-      <div className={`text-center ${statusClass} font-semibold`}>{status}</div>
+      <div className="text-center">
+        <span
+          className={`inline-block px-4 py-1 rounded-full text-white ${statusClass} font-semibold`}
+        >
+          {status}
+        </span>
+      </div>
       <div className="flex justify-center gap-5">
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/85692fa89ec6efbe236367c333447229988de5c950127aab2c346b9cdd885bdb?placeholderIfAbsent=true&apiKey=51ebf0c031414fe7a365d6657293527e"
           alt="Edit"
           className="cursor-pointer w-5 h-5"
-          onClick={() => openEditModal({ id, name, type, price, minGuests, maxGuests, status })}
+          onClick={() =>
+            openEditModal({
+              id,
+              name,
+              type,
+              price,
+              minGuests,
+              maxGuests,
+              status,
+            })
+          }
         />
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/12c72d419b305d862ab6fa5862b71d422877c54c8665826d40efd0e2e2d1840e"
