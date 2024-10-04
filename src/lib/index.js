756a0bed879d6d0139b3e64c63976563cdc29303
LouisKien <louisnamu02@gmail.com>
2024-10-04 21:55:21 +0700

    payment history page

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 import headerConstants from "./service/data/constants/headerConstants";
 import BookingService from "./service/bookingService";
+import paymentHistoryService from "./service/paymentHistoryService";
 
 
-
-export { headerConstants, BookingService };
+export { headerConstants, BookingService, paymentHistoryService };
