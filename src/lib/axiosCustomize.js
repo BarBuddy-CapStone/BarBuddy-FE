284474e80c6d01ac2c51539c2f7a600d589de788
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-10-22 23:26:57 +0700

    up thêm tên miền

--- src/lib/axiosCustomize.js
+++ src/lib/axiosCustomize.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://localhost:7069/",  // Đảm bảo sử dụng HTTPS
+  // serverURL: "https://barbuddy.id.vn/"
 });
 
 export default instance;
