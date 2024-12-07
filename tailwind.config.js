c3b2caa3370bad66ffc5f5aca67f2ebc0f7c528b
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-12-07 19:52:33 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,40 @@
 const { nextui } = require("@nextui-org/react");
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-      aBeeZee: ['aBeeZee', 'sans-serif'],
-      notoSansSC: ['Noto Sans SC', 'sans-serif'],
-    },
     extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
+      fontFamily: {
+        aBeeZee: ['aBeeZee', 'sans-serif'], // Adding aBeeZee as a custom font
+        notoSansSC: ['Noto Sans SC', 'sans-serif'], // Adding Noto Sans SC
       },
       keyframes: {
         scroll: {
           '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(-100%)' },
-        }
+        },
       },
       animation: {
         scroll: 'scroll 20s linear infinite',
-      }
-    }
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 20s linear infinite',
+      },
+    },
   },
   darkMode: "class",
   plugins: [
     nextui(),
     require('@tailwindcss/aspect-ratio')
   ],
-}
+};
